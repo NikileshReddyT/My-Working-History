47b99ea9c345796ddac8adf54698cb18268fefc5
NikileshReddyT <nikileshreddyt@gmail.com>
2024-10-14 12:46:35 +0530

    backend added with spring boot

---
+++ exit_portal_backend/src/main/java/com/jfsd/exit_portal_backend/ExitPortalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jfsd.exit_portal_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExitPortalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExitPortalBackendApplication.class, args);
+	}
+
+}
---
+++ exit_portal_backend/src/test/java/com/jfsd/exit_portal_backend/ExitPortalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfsd.exit_portal_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExitPortalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
