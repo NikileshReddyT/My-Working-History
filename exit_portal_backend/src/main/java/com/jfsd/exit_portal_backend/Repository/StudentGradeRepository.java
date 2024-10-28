9a32d39057715e67d8c1b809471b2dc28c5d3d1d
VUPPALASAIPRIYA <162854784+VUPPALASAIPRIYA@users.noreply.github.com>
2024-10-28 15:56:21 +0530

    added backend of student grade repository

--- exit_portal_backend/src/main/java/com/jfsd/exit_portal_backend/Repository/StudentGradeRepository.java
+++ exit_portal_backend/src/main/java/com/jfsd/exit_portal_backend/Repository/StudentGradeRepository.java
@@ -1,11 +1,17 @@
 package com.jfsd.exit_portal_backend.Repository;
 
-import com.jfsd.exit_portal_backend.Model.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.jfsd.exit_portal_backend.Model.StudentGrade;
 
 public interface StudentGradeRepository extends JpaRepository<StudentGrade, Long> {
     List<StudentGrade> findByUniversityId(String universityId);
     List<StudentGrade> findByUniversityIdAndCategory(String universityId, String category);
+
+
+    @Query("SELECT DISTINCT s.universityId FROM StudentGrade s")
+    List<String> findAllUniqueStudentIds();
 }
