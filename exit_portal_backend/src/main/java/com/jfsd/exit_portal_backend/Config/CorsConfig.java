027975bcc2f4e0c2fba4263fea79a19aa309aa1e
NikileshReddyT <nikileshreddyt@gmail.com>
2024-11-01 21:37:26 +0530

    added new domain name to the backend

--- exit_portal_backend/src/main/java/com/jfsd/exit_portal_backend/Config/CorsConfig.java
+++ exit_portal_backend/src/main/java/com/jfsd/exit_portal_backend/Config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://127.0.0.1:5500","http://localhost:5173","https://exit-portal-requirement-klu.vercel.app/")  // Update this based on your front-end URL
+                    .allowedOrigins("http://127.0.0.1:5500","http://localhost:5173","https://exit-portal-requirement-klu.vercel.app/","https://exitportal-klu.vercel.app/")  // Update this based on your front-end URL
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
                     .allowCredentials(true)
