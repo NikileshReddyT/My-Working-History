74578bfc9ed3755aeda09ebac065ee032676e864
NikileshReddyT <nikileshreddyt@gmail.com>
2024-12-13 22:36:13 +0530

    added vercel analytics to the website for webpage analytics

--- Exit-Portal-Frontend/src/App.jsx
+++ Exit-Portal-Frontend/src/App.jsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import Login from './components/Login';
-import Dashboard from './components/Dashboard';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-function App() {
+import React from "react";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
+function App() {
   return (
     <BrowserRouter>
+      <Analytics />
       <Routes>
         <Route path='/' element={<Login />} />
         <Route path='/dashboard' element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
-  )
-
-
-
-
+  );
 }
 
 export default App;
